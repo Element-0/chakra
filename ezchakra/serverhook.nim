50c78205bc6d928de3b700146aea13ae7964a136
CodeHz <codehz@outlook.com>
2021-01-27 02:29:18 +0800

    add DedicatedServer hook

---
+++ ezchakra/serverhook.nim
@@ -0,0 +1,29 @@
+{.deadCodeElim: on.}
+
+type
+  DedicatedServer* = distinct pointer
+  DedicatedServerHook* = proc(server: DedicatedServer) {.closure.}
+
+when defined(chakra):
+  import std/options, hookmc, importmc, cppinterop/cppstr
+
+  var hooks: seq[DedicatedServerHook]
+  var started: Option[DedicatedServer]
+
+  proc startServer(server: DedicatedServer, str: ptr CppString): int {.hookmc: "?start@DedicatedServer@@QEAA?AW4StartResult@1@AEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@@Z".} =
+    started = some server
+    for item in hooks:
+      item(server)
+    hooks.setLen 0
+    result = server.startServer_origin(str)
+
+  proc addServerHook*(hook: DedicatedServerHook) {.exportc, dynlib.} =
+    if started.isSome():
+      hook started.unsafeGet()
+    else:
+      hooks.add hook
+else:
+  proc addServerHook*(hook: DedicatedServerHook) {.importc, dynlib: "chakra.dll".}
+
+proc addServerHook*(raw: proc(server: DedicatedServer) {.cdecl.}) =
+  addServerHook do(server: DedicatedServer): raw(server)
